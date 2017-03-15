e78d7f65876f791767be574aa1ecfcbfd3dd581c
Harris Brakmic <brakmic@web.de>
2017-03-15 23:05:51 +0100

    replaced JS with TS, + two bugfixes

---
+++ src/local.ts
@@ -0,0 +1,238 @@
+import { Node, Commands, 
+         Constants, MessagesQueue, 
+         EventDOMNodeAttributes, 
+         Pipe}  from './common'
+         
+class LocalContainer {
+  public index;
+  constructor(public queueIndex, 
+              public domElement, 
+              public name) {
+    this.domElement = domElement;
+    this.name = name;
+    this.queueIndex = queueIndex;
+    this.index = null;
+  }
+}
+
+const containersByQueueAndName = {};
+const queuesByIndex = {};
+const elementsByQueue = {};
+const eventsByQueueAndName = {};
+const nativeInvocationsByQueue = {};
+let win = null;
+let doc = null;
+
+function setWindow(windowObj) {
+  win = windowObj;
+  doc = windowObj.document;
+}
+
+if (typeof window !== 'undefined') {
+  setWindow(window);
+}
+
+function createContainer(queueIndex, domElement, name) {
+  name = name || Constants.DEFAULT_NAME;
+  const res = new LocalContainer(queueIndex, domElement, name);
+  containersByQueueAndName[queueIndex][name] = res;
+  return res;
+}
+
+function serializeEventVal(queueIndex, val) {
+  if (val === win) {
+    return Constants.WINDOW;
+  } else if (val === doc) {
+    return Constants.DOCUMENT;
+  } else if (val instanceof win.Node) {
+    if (val[Constants.QUEUE_INDEX] === queueIndex) {
+      return val[Constants.NODE_INDEX];
+    }
+    ;
+  } else if (val instanceof Array) {
+    return val.map((v) => serializeEventVal(queueIndex, v));
+  } else if (typeof val === 'number' || typeof val === 'string' || typeof val === 'boolean') {
+    return val;
+  } else if (typeof val === 'function') {
+    return null;
+  }
+  return;
+}
+
+function generalEventHandler(queueIndex, evtTarget, evtName, ev) {
+  // console.log('generalEventHandler', arguments)
+  const queue = queuesByIndex[queueIndex];
+  const evtJSON = {extraData: {}};
+  const path = ev.path || EventDOMNodeAttributes.map((field) => ev[field]).filter((x) => x);
+  path.forEach(node => {
+    evtJSON.extraData[serializeEventVal(queueIndex, node)] = {
+      $value: node.value,
+      type: node.type,
+      checked: node.checked
+    };
+  });
+
+  for (var field in ev) {
+    evtJSON[field] = serializeEventVal(queueIndex, ev[field])
+  }
+
+
+  queuesByIndex[queueIndex].push([evtTarget, evtName, evtJSON]);
+  // console.log('evtJSON',evtJSON.type, evtJSON);
+  if (evtName === 'submit') {
+    ev.preventDefault();
+  }
+}
+
+function applyMessages(queueIndex, messages) {
+  const elements = elementsByQueue[queueIndex];
+  const containers = containersByQueueAndName[queueIndex];
+  const events = eventsByQueueAndName[queueIndex];
+  const nativeInvocations = nativeInvocationsByQueue[queueIndex];
+  //console.log('applyMessages', queueIndex, messages);
+  messages.forEach(msg => {
+    const msgType = msg[0];
+    //console.log('applyMessage:', msg);
+    switch (msgType) {
+      case (Commands.createContainer):
+      elements[msg[1]] = containers[msg[2]].domElement;
+      break;
+      case (Commands.createElement):
+        elements[msg[1]] = doc.createElement(msg[2].toLowerCase());
+        elements[msg[1]][Constants.QUEUE_INDEX] = queueIndex;
+        elements[msg[1]][Constants.NODE_INDEX] = msg[1];
+        break;
+      case (Commands.createTextNode):
+      elements[msg[1]] = doc.createTextNode(msg[2]);
+      elements[msg[1]][Constants.QUEUE_INDEX] = queueIndex;
+      elements[msg[1]][Constants.NODE_INDEX] = msg[1];
+      break;
+      case (Commands.createComment):
+      elements[msg[1]] = doc.createComment(msg[2]);
+      elements[msg[1]][Constants.QUEUE_INDEX] = queueIndex;
+      elements[msg[1]][Constants.NODE_INDEX] = msg[1];
+      break;
+      case (Commands.createDocumentFragment):
+      elements[msg[1]] = doc.createDocumentFragment(msg[2]);
+      break;
+      case (Commands.appendChild):
+      elements[msg[1]].appendChild(elements[msg[2]]);
+      break;
+      case (Commands.insertBefore):
+      elements[msg[1]].insertBefore(elements[msg[2]], msg[3] ? elements[msg[3]] : null);
+      break;
+      case (Commands.removeChild):
+      elements[msg[1]].removeChild(elements[msg[2]]);
+      break;
+        case (Commands.replaceChild):
+          elements[msg[1]].replaceChild(elements[msg[2]], elements[msg[3]]);
+        break;
+      case (Commands.setAttribute):
+      elements[msg[1]].setAttribute(msg[2], msg[3]);
+      break;
+      case (Commands.removeAttribute):
+      elements[msg[1]].removeAttribute(msg[2]);
+      break;
+      case (Commands.setStyles):
+      elements[msg[1]].style = msg[2];
+      break;
+      case (Commands.setStyle):
+      elements[msg[1]].style[msg[2]] = msg[3];
+      break;
+      case (Commands.innerHTML):
+      elements[msg[1]].innerHTML = msg[2];
+      break;
+      case (Commands.innerText):
+      elements[msg[1]].innerText = msg[2];
+      break;
+      case (Commands.textContent):
+      elements[msg[1]].textContent = msg[2];
+      break;
+      case (Commands.setValue):
+      elements[msg[1]].value = msg[2];
+      break;
+      case (Commands.pause):
+        elements[msg[1]].pause();
+        break;
+      case (Commands.play):
+        elements[msg[1]].play();
+        break;
+      case (Commands.src):
+        elements[msg[1]].src = msg[2];
+      case (Commands.addEventListener):
+      const func = generalEventHandler.bind(null, queueIndex, msg[1], msg[2]);
+      events[msg[2]] = events[msg[2]] || {};
+      events[msg[2]][msg[3]] = func;
+      elements[msg[1]].addEventListener(msg[2], func, msg[4]);
+      break;
+      case (Commands.removeEventListener):
+      events[msg[2]] = events[msg[2]] || {};
+      const origFunc = events[msg[2]][msg[3]];
+      elements[msg[1]].removeEventListener(msg[2], origFunc);
+      break;
+      case (Commands.initiated):
+        handleRemoteInit(queueIndex);
+        break;
+      case (Commands.invokeNative):
+      if (nativeInvocations[msg[2]]) {
+        nativeInvocations[msg[2]](elements[msg[1]], msg[3]);
+      }
+      break;
+    }
+  });
+}
+
+function handleRemoteInit(queueIndex) {
+  updateRemoteOnInit(queueIndex);
+  registerToWindowChanges(() => updateRemoteOnInit(queueIndex));
+}
+
+function updateRemoteOnInit(queueIndex) {
+  queuesByIndex[queueIndex].push([Constants.WINDOW, 'updateProperties', {
+    extraData: {
+      WINDOW: {
+        screen: {
+          width: win.screen.width,
+          height: win.screen.height,
+          deviceXDPI: win.screen.deviceXDPI,
+          logicalXDPI: win.screen.logicalXDPI,
+          orientation: {
+            angle: win.screen.orientation && win.screen.orientation.angle,
+            type: win.screen.orientation && win.screen.orientation.type
+          }
+        },
+        devicePixelRatio: win.devicePixelRatio,
+        innerWidth: win.innerWidth,
+        innerHeight: win.innerHeight
+      }
+    }
+  }]);
+}
+
+function registerToWindowChanges(callback) {
+  win.addEventListener('orientationchange', callback);
+  win.addEventListener('resize', callback);
+}
+
+function createMessageQueue(channel, timerFunction, nativeInvocations) {
+  if (!win) {
+    throw 'Please setWindow before create message queues';
+  }
+  const queue = new MessagesQueue();
+  const queueIndex = queue.index;
+  queuesByIndex[queueIndex] = queue;
+  containersByQueueAndName[queueIndex] = {};
+  elementsByQueue[queueIndex] = {};
+  nativeInvocationsByQueue[queueIndex] = nativeInvocations || {};
+  elementsByQueue[queueIndex][Constants.DOCUMENT] = doc;
+  elementsByQueue[queueIndex][Constants.WINDOW] = win;
+  eventsByQueueAndName[queueIndex] = {};
+  queue.setPipe(channel, applyMessages.bind(null, queueIndex), timerFunction);
+  return queueIndex;
+}
+
+export {
+  createContainer,
+  createMessageQueue,
+  setWindow
+}
---
+++ src/remote.ts
@@ -0,0 +1,397 @@
+import { Node, Commands, 
+        Constants, MessagesQueue, 
+        StyleAttributes, EventDOMNodeAttributes, 
+        SupportedEvents, Pipe}  from './common'
+
+let index = 0;
+
+const queue = new MessagesQueue();
+const eventsByTypeAndTarget = {};
+const connectedElementsByIndex = {};
+
+class RemoteStyle {
+  constructor(private $index, private $values) {
+    this.$index = $index;
+    this.$values = {};
+  }
+}
+
+StyleAttributes.forEach((k) => {
+  Object.defineProperty(RemoteStyle.prototype, k, {
+    set: function (val) {
+      queue.push([Commands.setStyle, this.$index, k, val]);
+      this.$values[k] = val;
+    },
+    get: function () {
+      return this.$values[k];
+    }
+  })
+})
+
+
+class RemoteNode {
+  public nodeType;
+  public $index;
+  public $host;
+  public parentNode;
+  public childNodes;
+  public $value;
+  public $textContent;
+  constructor(nodeType?) {
+    index++;
+    this.nodeType = nodeType;
+    this.$index = index;
+    this.$host = null;
+    this.parentNode = null;
+    this.childNodes = [];
+  }
+
+  get children() {
+    return this.childNodes;
+  }
+
+  get firstChild() {
+    return this.childNodes[0];
+  }
+
+  get nextSibling() {
+    if (!this.parentNode) {
+      return null;
+    }
+    const idx = this.parentNode.childNodes.indexOf(this);
+    if (idx === -1 || idx === this.parentNode.childNodes.length - 1) {
+      return null;
+    }
+    return this.parentNode.childNodes[idx + 1];
+  }
+
+  get prevSibling() {
+    if (!this.parentNode) {
+      return null;
+    }
+    const idx = this.parentNode.childNodes.indexOf(this);
+    if (idx === -1 || idx === 0) {
+      return null;
+    }
+    return this.parentNode.childNodes[idx - 1];
+  }
+
+  get ownerDocument() {
+    return document
+  }
+
+  set innerHTML(val) {
+  }
+
+  set host(host) {
+    if (Boolean(host) === Boolean(this.$host)) {
+      return;
+    }
+    this.$host = host;
+    if (host) {
+      connectedElementsByIndex[this.$index] = this;
+    } else {
+      delete connectedElementsByIndex[this.$index];
+    }
+    this.childNodes.forEach((child) => {
+      child.host = host
+    });
+  }
+
+  appendChild(child) {
+    queue.push([Commands.appendChild, this.$index, child.$index]);
+    child.parentNode = this;
+    this.childNodes.push(child);
+    child.host = this.$host;
+  }
+
+  insertBefore(child, refChild) {
+    queue.push([Commands.insertBefore, this.$index, child.$index, refChild ? refChild.$index : null]);
+    const idx = refChild ? this.childNodes.indexOf(refChild) : this.childNodes.length;
+    child.parentNode = this;
+    this.childNodes.splice(idx, 0, child);
+    child.host = this.$host;
+    return child;
+  }
+
+  removeChild(child) {
+    queue.push([Commands.removeChild, this.$index, child.$index]);
+    const idx = this.childNodes.indexOf(child)
+    if (idx !== -1) {
+      this.childNodes.splice(idx, 1);
+    }
+    child.host = null;
+  }
+
+  replaceChild(newChild, oldChild) {
+    queue.push([Commands.replaceChild, this.$index, newChild.$index, oldChild.$index]);
+    const idx = this.childNodes.indexOf(oldChild)
+    if (idx !== -1) {
+      this.childNodes.splice(idx, 1, newChild);
+    }
+    newChild.parentNode = this;
+    newChild.host = this.$host;
+    oldChild.host = null;
+  }
+
+  addEventListener(evtType, callback, capture) {
+    addEventListener(this.$index, evtType, callback, capture);
+  }
+
+  removeEventListener(evtType, callback) {
+    removeEventListener(this.$index, evtType, callback);
+  }
+
+  set value(val) {
+    this.$value = val;
+    queue.push([Commands.setValue, this.$index, val]);
+  }
+
+  get value() {
+    return this.$value;
+  }
+
+  get textContent() {
+    return this.$textContent;
+  }
+
+  set textContent(val) {
+    queue.push([Commands.textContent, this.$index, val]);
+  }
+
+  invokeNative(name, args) {
+    queue.push([Commands.invokeNative, this.$index, name, args]);
+  }
+}
+
+class RemoteTextualNode extends RemoteNode {
+  constructor(nodeType, val) {
+    super(Node.TEXT_NODE);
+  }
+
+  set nodeValue(val) {
+    queue.push([Commands.textContent, this.$index, val]);
+  }
+}
+
+class RemoteComment extends RemoteTextualNode {
+  constructor(val) {
+    super(Node.COMMENT_NODE, val);
+  }
+}
+
+class RemoteText extends RemoteTextualNode {
+  constructor(val?) {
+    super(Node.TEXT_NODE, val);
+  }
+}
+
+class RemoteElement extends RemoteNode {
+  public tagName;
+  public $style;
+  public $attr;
+  constructor(tagName) {
+    super(Node.ELEMENT_NODE);
+    this.tagName = tagName.toUpperCase();
+    this.$style = new RemoteStyle(this.$index, undefined);
+    this.$attr = {};
+  }
+
+  get nodeName() {
+    return this.tagName;
+  }
+
+  setAttribute(k, v) {
+    queue.push([Commands.setAttribute, this.$index, k, v]);
+    this.$attr[k] = {name: k, value: v};
+    this[k] = v;
+  }
+
+  removeAttribute(k) {
+    queue.push([Commands.removeAttribute, this.$index, k]);
+    delete this.$attr[k];
+  }
+
+  get style() {
+    return this.$style;
+  }
+
+  set style(val) {
+    queue.push([Commands.setStyle, this.$index, val]);
+  }
+
+  set innerHTML(val) {
+    queue.push([Commands.innerHTML, this.$index, val]);
+  }
+
+  set innerText(val) {
+    queue.push([Commands.innerText, this.$index, val]);
+  }
+}
+
+
+class RemoteContainer extends RemoteElement {
+  constructor() {
+    super('div');
+    this.$host = this.$index;
+  }
+}
+
+class RemoteFragment extends RemoteNode {
+  constructor() {
+    super(Node.DOCUMENT_FRAGMENT_NODE);
+  }
+}
+
+class RemoteVideo extends RemoteElement {
+  public $src;
+    constructor() {
+        super('video');
+    }
+
+    pause() {
+      queue.push([Commands.pause, this.$index]);
+    }
+
+    play() {
+      queue.push([Commands.play, this.$index]);
+    }
+
+    get src() {
+      return this.$src;
+    }
+
+    set src(value) {
+      this.$src =  value;
+      queue.push([Commands.src, this.$index, value]);
+    }
+}
+
+function createElement(tagName) {
+  if (tagName === 'video') {
+    return createVideoNode()
+  }
+  const res = new RemoteElement(tagName);
+  queue.push([Commands.createElement, res.$index, res.tagName]);
+  return res;
+}
+
+function createTextNode(val) {
+  const res = new RemoteText();
+  queue.push([Commands.createTextNode, res.$index, val]);
+  return res;
+}
+
+function createComment(val) {
+  const res = new RemoteNode();
+  queue.push([Commands.createComment, res.$index, val]);
+  return res;
+}
+
+function createVideoNode() {
+  const res = new RemoteVideo();
+  queue.push([Commands.createElement, res.$index, res.tagName]);
+  return res;
+}
+
+function createDocumentFragment() {
+    const res = new RemoteFragment();
+    queue.push([Commands.createDocumentFragment, res.$index]);
+    return res;
+}
+
+export function createContainer(name) {
+  name = name || Constants.DEFAULT_NAME;
+  const res = new RemoteContainer();
+  connectedElementsByIndex[res.$index] = res;
+  queue.push([Commands.createContainer, res.$index, name]);
+  return res;
+}
+
+function addEventListener(target, evtName, callback, capture) {
+  index++;
+  //console.log('addEventListener', target, evtName);
+  eventsByTypeAndTarget[evtName] = eventsByTypeAndTarget[evtName] || {};
+  eventsByTypeAndTarget[evtName][target] = eventsByTypeAndTarget[evtName][target] || {};
+  eventsByTypeAndTarget[evtName][target][index] = callback;
+  queue.push([Commands.addEventListener, target, evtName, index, capture]);
+}
+
+function removeEventListener(target, evtName, callback) {
+  // console.log('addEventListener', target, evtName);
+  eventsByTypeAndTarget[evtName] = eventsByTypeAndTarget[evtName] || {};
+  eventsByTypeAndTarget[evtName][target] = eventsByTypeAndTarget[evtName][target] || {};
+  const evts = eventsByTypeAndTarget[evtName][target];
+  const idx = Object.keys(evts).find((evtIndex) => {
+    return evts[evtIndex] === callback;
+  });
+  delete evts[idx];
+  queue.push([Commands.removeEventListener, target, evtName, index]);
+}
+
+function handleMessagesFromPipe(messages) {
+  messages.forEach(msg => {
+    const evtTarget = msg[0];
+    const evtName = msg[1];
+    const evtJSON = msg[2];
+    Object.keys(evtJSON.extraData).forEach((index) => {
+      if (connectedElementsByIndex[index] && evtJSON.extraData[index]) {
+        Object.assign(connectedElementsByIndex[index], evtJSON.extraData[index]);
+      }
+    })
+    EventDOMNodeAttributes.forEach((field) => {
+      evtJSON[field] = (evtJSON[field] instanceof Array) ? evtJSON[field].map(val => connectedElementsByIndex[val]) : connectedElementsByIndex[evtJSON[field]];
+    });
+    // console.log(evtJSON);
+
+    if (eventsByTypeAndTarget[evtName] && eventsByTypeAndTarget[evtName][evtTarget]) {
+      Object.keys(eventsByTypeAndTarget[evtName][evtTarget]).forEach((callbackIndex) => {
+        eventsByTypeAndTarget[evtName][evtTarget][callbackIndex](evtJSON);
+      })
+    }
+  });
+}
+
+export function setChannel(channel, timerFunction) {
+  queue.setPipe(channel, handleMessagesFromPipe, timerFunction);
+  onInit();
+}
+
+function onInit() {
+  queue.push([Commands.initiated]);
+}
+
+export const document = {
+  createElement,
+  createTextNode,
+  createComment,
+  createDocumentFragment,
+  addEventListener: addEventListener.bind(null, Constants.DOCUMENT),
+  removeEventListener: removeEventListener.bind(null, Constants.DOCUMENT),
+  documentElement: new RemoteElement('html')
+};
+
+SupportedEvents.forEach((e) => {
+  document[e] = addEventListener.bind(null, Constants.DOCUMENT, e.substr(2));
+});
+
+export const window = {
+  addEventListener: addEventListener.bind(null, Constants.WINDOW),
+  removeEventListener: removeEventListener.bind(null, Constants.WINDOW),
+  document: document,
+  location: {href: 'https://localhost', protocol: 'https:'},
+  navigator: {
+    userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.98 Safari/537.36'
+  },
+  top: this
+}
+
+connectedElementsByIndex[Constants.WINDOW] = window;
+connectedElementsByIndex[Constants.DOCUMENT] = document;
+
+// exports {
+//   document,
+//   window,
+//   createContainer,
+//   setChannel
+// }
